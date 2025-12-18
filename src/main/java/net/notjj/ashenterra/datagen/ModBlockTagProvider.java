e4ef97ecb697c1cdc0dc860f9211afc3e0461c9b
NotJJ <z.notjj@gmail.com>
2025-12-18 20:36:20 +1100

    Little block tag stuff

--- src/main/java/net/notjj/ashenterra/datagen/ModBlockTagProvider.java
+++ src/main/java/net/notjj/ashenterra/datagen/ModBlockTagProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
 import net.notjj.ashenterra.block.ModBlocks;
 import net.notjj.ashenterra.util.ModTags;
 
@@ -40,24 +41,29 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.DEEPSLATE_LEAD_ORE)
                 .add(ModBlocks.DEEPSLATE_TIN_ORE);
 
-        //getOrCreateTagBuilder(ModTags.Blocks.NEEDS_COPPER_TOOL);
+        getOrCreateTagBuilder(ModTags.Blocks.NEEDS_COPPER_TOOL)
+                .addTag(BlockTags.NEEDS_STONE_TOOL)
+                .add(Blocks.IRON_ORE)
+                .add(Blocks.DEEPSLATE_IRON_ORE)
+                .add(ModBlocks.SILVER_ORE)
+                .add(ModBlocks.DEEPSLATE_SILVER_ORE);;
 
         getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
-                .add(ModBlocks.SILVER_ORE)
-                .add(ModBlocks.DEEPSLATE_SILVER_ORE)
                 .add(ModBlocks.TUNGSTEN_ORE)
                 .add(ModBlocks.DEEPSLATE_TUNGSTEN_ORE)
                 .add(ModBlocks.PLATINUM_ORE)
                 .add(ModBlocks.DEEPSLATE_PLATINUM_ORE)
                 .add(ModBlocks.METEORITE);
 
-        //getOrCreateTagBuilder(ModTags.Blocks.NEEDS_SILVER_TOOL);
+        getOrCreateTagBuilder(ModTags.Blocks.NEEDS_SILVER_TOOL)
+                .addTag(BlockTags.NEEDS_IRON_TOOL);
 
-        //getOrCreateTagBuilder(ModTags.Blocks.NEEDS_GOLD_TOOL)
-        //        .add(ModBlocks.DEMONITE_ORE)
-        //        .add(ModBlocks.CRIMTANE_ORE);
+        getOrCreateTagBuilder(ModTags.Blocks.NEEDS_GOLD_TOOL)
+                .addTag(BlockTags.NEEDS_DIAMOND_TOOL);
 
         getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.DEMONITE_ORE)
+                .add(ModBlocks.CRIMTANE_ORE)
                 .add(ModBlocks.HELLSTONE_ORE);
 
     }
